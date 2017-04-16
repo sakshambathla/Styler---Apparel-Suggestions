9e7db10e7b2a78c88866363bb82d35a2a53635f5
kapardi <kaparditrivedi@gmail.com>
2017-04-16 09:45:23 +0530

    different frontend bro

--- action_page.php
+++ action_page.php
@@ -1,5 +1,27 @@
 <html>
+<style>
+#ex4 {
+	width: 800px;
+	margin: 0 auto;
+}
 
+img {
+	margin: 20px;
+	border: 5px solid #eee;
+	-webkit-box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+	-moz-box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+	box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+	-webkit-transition: all 0.5s ease-out;
+	-moz-transition: all 0.5s ease;
+	-o-transition: all 0.5s ease;
+}
+
+img:hover {
+	-webkit-transform: rotate(-7deg);
+	-moz-transform: rotate(-7deg);
+	-o-transform: rotate(-7deg);
+} 
+</style>
 <body background="action_page.jpg">
 <center>
 <h1><b>Welcome</b></h1> 
